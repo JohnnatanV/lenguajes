f9f3a8575cba3a60dbd1cf4a4e797973eaed4576
JohnnatanV <joh.villaneda@outlook.com>
2025-02-01 12:36:35 -0500

    Update python classes

--- PYTHON/03-funciones/07-ejercicios.py
+++ PYTHON/03-funciones/07-ejercicios.py
@@ -1,12 +1,52 @@
 # Define una funcion que verifique palindromos
 
 def es_palindromo(text):
-    return ""
+    text = erase_space(text)
+    new_text = reverse2(text)
+    return new_text.lower() == text.lower()
+
+
+def erase_space(text):
+    newText = ""
+    for char in text:
+        if char != " ":
+            newText += char
+
+    return newText
+
+
+def reverse(text):
+    newText = ""
+    length = len(text)-1
+
+    for _ in text:
+        if length >= 0:
+            newText += text[length]
+            length = length - 1
+
+    return newText
+
+
+def reverse2(text):
+    reverse_text = ""
+    for char in text:
+        reverse_text = char + reverse_text
+
+    return reverse_text
+
+# print("Amo la Paloma", erase_space("Amo la Paloma"))
+# print("Hola mundo", reverse("Hola mundo"))
 
 
 print("Abba", es_palindromo("Abba"))
-# print("Reconocer", es_palindromo("Reconocer"))
-# print("Amo la Paloma", es_palindromo("Amo la Plaoma"))
-# print("Hola mundo", es_palindromo("Hola mundo"))
+print("Reconocer", es_palindromo("Reconocer"))
+print("Amo la Paloma", es_palindromo("Amo la Paloma"))
+print("Hola mundo", es_palindromo("Hola mundo"))
 
-# 3:32:18
+# print("Hola mundo".replace(" ", ""))
+# print("HOla MunDo".lower().replace(" ", ""))
+
+text = "Amo la Paloma"
+newText = text.lower().replace(" ", "")
+
+# print((text[12]), len(newText))
---
+++ PYTHON/04-tipos-avanzados/01-listas.py
@@ -0,0 +1,6 @@
+numeros = [1, 2, 3]
+letras = ["nombre", "apellido", "id"]
+datos = {}
+# 3:44:44
+
+print(letras)
