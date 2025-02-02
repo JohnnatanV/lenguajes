dbee53855f263e1d70d4ef1f3a5ca21d0bc7c3ea
Johnnatan Villaneda <joh.villaneda@outlook.com>
2025-02-02 00:36:13 -0500

    Updating python course 02/02/2025

--- PYTHON/04-tipos-avanzados/01-listas.py
+++ PYTHON/04-tipos-avanzados/01-listas.py
@@ -1,6 +1,17 @@
-numeros = [1, 2, 3]
-letras = ["nombre", "apellido", "id"]
+numeros = [1, 2, 3, 4.5, 1000]
+letras = ["a", "b", "C", "D"]
+palabras = ["nombre", "apellido", "id"]
+nombres = ["Johnnatan Villaneda", "Kelly Murillas"]
+boleanos = [True, False]
+matriz = [[0, 1], [1, 0], ["A", "Z"]]
+ceros = [0] * 10
+cerosyunos = [0, 1] * 10
+alfanumerico = numeros + letras
+rango = list(range(1, 11))
+chars = list("Hola mundo")
+
 datos = {}
 # 3:44:44
 
-print(letras)
+# print(letras, palabras, nombres, datos, boleanos, matriz)
+print(chars)
---
+++ PYTHON/04-tipos-avanzados/02-manipulando-listas.py
@@ -0,0 +1,42 @@
+mascotas = ["Killer", "Lucifer", "Judas", "Cremita"]
+
+# for mascota in mascotas:
+#   print(mascota)
+
+
+# print(mascotas[3])
+
+mascotas[3] = "Vainilla"
+
+# print(mascotas)
+# print(mascotas[3])
+
+# print(mascotas[-1])
+
+# print(mascotas[1:4:2])
+
+
+numeros = list(range(21))
+numeros2 = list(range(1, 21))
+
+print(numeros[3:10])
+
+print(numeros[5:])
+
+print(numeros[:19])
+
+print(numeros[-10])
+
+print(numeros[::2])  # Numeros pares
+
+print(numeros[2::])
+
+# Todos los caminos llevan a roma, mas de un metodo para obtener el mismo resultado
+print(numeros[1::2])  # numeros impares
+
+print(numeros2[::2])  # numeros impares
+
+
+###
+
+print(numeros[2:19:4])
